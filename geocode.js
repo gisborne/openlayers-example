00e4be1a8658e9a1ea4e1f79e184b810a9cd02ec
Guyren Howe <guyren@relevantlogic.com>
2015-06-22 16:11:35 -0700

    Add geocodeSerialized function to geocode.js, which is convenient to call using JQuery

--- geocode.js
+++ geocode.js
@@ -4,9 +4,15 @@ $(function() {
   function geocodeStr(addr) {
     return geocodeObj({q: addr})
   }
+
   function geocodeObj(addr) {
     return $.getJSON('http://nominatim.openstreetmap.org/search?format=json', addr)
   }
+
+  learnSD.geocodeSerialized = function geocodeSerialized(addr) {
+    return $.getJSON('http://nominatim.openstreetmap.org/search?format=json', addr)
+  }
+
   learnSD.geocode = function geocode(addr) {
     if (typeof(addr) === 'string') {
       return geocodeObj({q: addr})
